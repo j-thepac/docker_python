fd8399f5e4d846edfa2e621e586e2974273989d0
Deepak <j.thepac@gmail.com>
2021-01-02 14:26:36 +0530

    Update flask_example_deepak.py

--- 1_flask/flask_example_deepak.py
+++ 1_flask/flask_example_deepak.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 app = Flask(__name__)
 @app.route('/')
 def hello_world() -> str: return "Hello world"
-if __name__ == '__main__':app.run(debug=True, host='0.0.0.0' ,port = 8000)
+if __name__ == '__main__':app.run(debug=True, host='0.0.0.0' ,port = 5000)
 
 
 """
